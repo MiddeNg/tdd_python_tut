3dc01277570122c468cca5479a2d330c2836a0f6
William Ng <william23282328@gmail.com>
2023-03-21 14:45:22 +0800

    new URL, view and template to display lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -97,7 +97,8 @@ class NewVisitorTest(LiveServerTestCase):
         ## We use a new browser session to make sure that no information
         ## of Edith's is coming through from cookies etc
         self.browser.quit()
-        self.browser = webdriver.Firefox()
+
+        self.setUp()
 
         # Francis visits the home page.  There is no sign of Edith's list
         self.browser.get(self.live_server_url)
