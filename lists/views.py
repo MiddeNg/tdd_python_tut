5c89b479f57e1a59f6b332ee9d1005ed53eae2e1
William Ng <william23282328@gmail.com>
2023-03-26 14:43:46 +0800

    check dup list item

--- lists/views.py
+++ lists/views.py
@@ -1,21 +1,20 @@
 from django.shortcuts import redirect, render
 from lists.models import Item, List
 from django.core.exceptions import ValidationError
-from lists.forms import ItemForm
+from lists.forms import ExistingListItemForm, ItemForm
 
 # Create your views here.
 def home_page(request):
     return render(request, 'home.html', {'form': ItemForm()})
 
 def view_list(request, list_id):
-    list_ = List.objects.get(id=list_id)
     error = None
-    form = ItemForm()
-
+    list_ = List.objects.get(id=list_id)
+    form = ExistingListItemForm(for_list=list_)
     if request.method == 'POST':
-        form = ItemForm(data=request.POST)
+        form = ExistingListItemForm(for_list=list_, data=request.POST)
         if form.is_valid():
-            form.save(for_list=list_)
+            form.save()
             return redirect(list_)
     return render(request, 'list.html', {'list': list_, 'error': error, "form": form})
 
